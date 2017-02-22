66afa90bd2033707876e284376f1746274b4bb20
Shovit Kandel <shovit.kandel@gmail.com>
2017-02-22 12:40:01 -0600

    belt exam1

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get users_index_url
+    assert_response :success
+  end
+
+  test "should get login" do
+    get users_login_url
+    assert_response :success
+  end
+
+  test "should get register" do
+    get users_register_url
+    assert_response :success
+  end
+
+end
